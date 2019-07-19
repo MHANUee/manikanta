e7ad2e5a6870ca5cbbb4539bce78d1ad817a44ec
MHANUee <53078537+MHANUee@users.noreply.github.com>
2019-07-19 15:29:19 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,10 @@
+# manikanta#manikantakintali6@gmail.com
+number=int(input())
+if (number>0)
+    if (number%2=0)
+    
+    print("even"):
+        els
+            print("odd")
+            else:
+                print("invalid")
